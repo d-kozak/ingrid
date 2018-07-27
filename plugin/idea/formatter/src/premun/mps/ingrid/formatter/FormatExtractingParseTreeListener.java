ea4b3f6032e9b3adbdcd91e2862ee5ec28d36053
dkozak <dkozak94@gmail.com>
2018-07-27 15:39:31 +0200

    find alternatives algorithm, first version finished, seems to work, but need heavy refactoring and more testing

--- plugin/idea/formatter/src/premun/mps/ingrid/formatter/FormatExtractingParseTreeListener.java
+++ plugin/idea/formatter/src/premun/mps/ingrid/formatter/FormatExtractingParseTreeListener.java
@@ -1,14 +1,19 @@
 package premun.mps.ingrid.formatter;
 
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.tool.Grammar;
 import org.antlr.v4.tool.Rule;
 import premun.mps.ingrid.formatter.model.FormatInfo;
 import premun.mps.ingrid.formatter.model.FormatInfoMapKey;
-import premun.mps.ingrid.model.GrammarInfo;
-import premun.mps.ingrid.model.ParserRule;
+import premun.mps.ingrid.model.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 public class FormatExtractingParseTreeListener extends BaseParseTreeListener {
 
@@ -27,12 +32,40 @@ public class FormatExtractingParseTreeListener extends BaseParseTreeListener {
     public void enterEveryRule(ParserRuleContext parserRuleContext) {
         Rule antlrRule = grammar.getRule(parserRuleContext.getRuleIndex());
         ParserRule parserRule = (ParserRule) grammarInfo.rules.get(antlrRule.name);
-        //Alternative alternative = FormatExtractorUtils.findAppropriateAlternative(parserRule.alternatives, parserRuleContext.children, Arrays.asList(grammar.getRuleNames()));
-
-
         List<String> context = getContext(parserRuleContext);
+        String serialized = serializeChildren(parserRuleContext.children);
+        System.out.print(context + " => ");
+        System.out.println(serialized);
+        System.out.println("Choosing from alternatives: ");
+        List<String> alternatives = parserRule.alternatives.stream()
+                                                           .map(this::serializeAlternative)
+                                                           .collect(Collectors.toList());
+        for (String alternative : alternatives) {
+            System.out.println(alternative);
+        }
 
-        System.out.println(context);
+
+        Alternative appropriateAlternative = FormatExtractorUtils.findAppropriateAlternative(parserRule.alternatives, parserRuleContext.children, Arrays.asList(grammar.getRuleNames()));
+        System.out.println("appropriate alternative => " + serializeAlternative(appropriateAlternative));
+        System.out.println("");
+    }
+
+    private String serializeAlternative(Alternative alternative) {
+        return alternative.elements.stream()
+                                   .map(this::serializeElem)
+                                   .collect(Collectors.joining(" "));
+    }
+
+    private String serializeElem(RuleReference elem) {
+        if (elem.rule instanceof LiteralRule) {
+            return "'" + ((LiteralRule) elem.rule).value + "'";
+        } else return elem.rule.name;
+    }
+
+    private String serializeChildren(List<ParseTree> children) {
+        return children.stream()
+                       .map(this::serializeParseTree)
+                       .collect(joining(" "));
     }
 
     private List<String> getContext(ParserRuleContext parserRuleContext) {
@@ -45,5 +78,17 @@ public class FormatExtractingParseTreeListener extends BaseParseTreeListener {
 
     public Map<FormatInfoMapKey, FormatInfo> getFormatInfo() {
         return formatInfo;
+    }
+
+    private String serializeParseTree(ParseTree tree) {
+        if (tree instanceof TerminalNode) {
+            return "'" + ((TerminalNode) tree).getSymbol()
+                                              .getText() + "'";
+        } else if (tree instanceof RuleContext) {
+            return grammar.getRule(((RuleContext) tree).getRuleIndex()).name;
+        } else {
+            throw new IllegalArgumentException("Unknown tree type " + tree.getClass()
+                                                                          .getName());
+        }
     }
 }
--- plugin/idea/formatter/src/premun/mps/ingrid/formatter/FormatExtractorUtils.java
+++ plugin/idea/formatter/src/premun/mps/ingrid/formatter/FormatExtractorUtils.java
@@ -4,18 +4,18 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
-import premun.mps.ingrid.model.Alternative;
-import premun.mps.ingrid.model.LiteralRule;
-import premun.mps.ingrid.model.Quantity;
-import premun.mps.ingrid.model.RuleReference;
+import premun.mps.ingrid.model.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FormatExtractorUtils {
+import static java.util.stream.Collectors.toList;
 
+public class FormatExtractorUtils {
     public static Alternative findAppropriateAlternative(List<Alternative> alternatives, List<ParseTree> parserRuleContext, List<String> ruleNames) {
-        alternatives = new ArrayList<>(alternatives);
+        alternatives = alternatives.stream()
+                                   .map(AlternativeDTO::new)
+                                   .collect(toList());
         for (int i = 0; i < parserRuleContext.size(); i++) {
             if (alternatives.size() < 2) break;
             ParseTree parseTree = parserRuleContext.get(i);
@@ -26,21 +26,20 @@ public class FormatExtractorUtils {
                 alternativeLoop:
                 for (int j = alternatives.size() - 1; j >= 0; j--) {
                     Alternative alternative = alternatives.get(j);
-                    for (int k = i; k < alternative.elements.size(); k++) {
+                    for (int k = 0; k < alternative.elements.size(); k++) {
                         RuleReference reference = alternative.elements.get(k);
                         if (reference.rule instanceof LiteralRule) {
                             if (((LiteralRule) reference.rule).value.equals(text)) {
-                                break alternativeLoop;
+                                alternative.elements.remove(k);
+                                continue alternativeLoop;
                             } else {
                                 if (reference.quantity == Quantity.EXACTLY_ONE || reference.quantity == Quantity.AT_LEAST_ONE) {
-                                    alternatives.remove(alternative);
-                                    break alternativeLoop;
+                                    break;
                                 }
                             }
                         } else {
                             if (reference.quantity == Quantity.EXACTLY_ONE || reference.quantity == Quantity.AT_LEAST_ONE) {
-                                alternatives.remove(alternative);
-                                break alternativeLoop;
+                                break;
                             }
                         }
                     }
@@ -48,6 +47,31 @@ public class FormatExtractorUtils {
                     alternatives.remove(alternative);
                 }
             } else if (parseTree instanceof RuleNode) {
+                int ruleIndex = ((RuleNode) parseTree).getRuleContext()
+                                                      .getRuleIndex();
+                String ruleName = ruleNames.get(ruleIndex);
+
+                alternativeLoop:
+                for (int j = alternatives.size() - 1; j >= 0; j--) {
+                    Alternative alternative = alternatives.get(j);
+                    for (int k = 0; k < alternative.elements.size(); k++) {
+                        RuleReference reference = alternative.elements.get(k);
+                        if (reference.rule instanceof ParserRule) {
+                            if (reference.rule.name.equals(ruleName)) {
+                                alternative.elements.remove(k);
+                                continue alternativeLoop;
+                            } else if (reference.quantity == Quantity.EXACTLY_ONE || reference.quantity == Quantity.AT_LEAST_ONE) {
+                                break;
+                            }
+                        } else {
+                            if (reference.quantity == Quantity.EXACTLY_ONE || reference.quantity == Quantity.AT_LEAST_ONE) {
+                                break;
+                            }
+                        }
+                    }
+                    // didn't match
+                    alternatives.remove(alternative);
+                }
 
             } else {
                 throw new UnsupportedOperationException("Cannot process parse tree of type " + parseTree.getClass()
@@ -58,6 +82,17 @@ public class FormatExtractorUtils {
             throw new IllegalArgumentException("No appropriate alternative found");
         else if (alternatives.size() > 1)
             throw new IllegalArgumentException("More than one alternative found");
-        else return alternatives.get(0);
+        else {
+            return ((AlternativeDTO) alternatives.get(0)).original;
+        }
+    }
+
+    private static class AlternativeDTO extends Alternative {
+        private final Alternative original;
+
+        public AlternativeDTO(Alternative original) {
+            this.original = original;
+            this.elements = new ArrayList<>(original.elements);
+        }
     }
 }
