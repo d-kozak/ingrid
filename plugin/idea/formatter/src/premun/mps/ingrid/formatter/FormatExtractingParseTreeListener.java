2af306d582933102400739674b67a506145bcced
dkozak <dkozak94@gmail.com>
2018-07-30 13:08:44 +0200

    first version of the input to rule mapping algorithm

--- plugin/idea/formatter/src/premun/mps/ingrid/formatter/FormatExtractingParseTreeListener.java
+++ plugin/idea/formatter/src/premun/mps/ingrid/formatter/FormatExtractingParseTreeListener.java
@@ -2,6 +2,7 @@ package premun.mps.ingrid.formatter;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.tool.Grammar;
@@ -11,6 +12,7 @@ import premun.mps.ingrid.formatter.model.FormatInfoMapKey;
 import premun.mps.ingrid.model.*;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.joining;
@@ -44,10 +46,152 @@ public class FormatExtractingParseTreeListener extends BaseParseTreeListener {
             System.out.println(alternative);
         }
 
-
         Alternative appropriateAlternative = FormatExtractorUtils.findAppropriateAlternative(parserRule.alternatives, parserRuleContext.children, Arrays.asList(grammar.getRuleNames()));
         System.out.println("appropriate alternative => " + serializeAlternative(appropriateAlternative));
         System.out.println("");
+        List<ParseTree> copy = new ArrayList<>(parserRuleContext.children);
+        extractFormatInfo(copy, appropriateAlternative.elements);
+    }
+
+    private void extractFormatInfo(List<ParseTree> ast, List<RuleReference> elements) {
+        for (RuleReference ruleReference : elements) {
+            match(ruleReference, ast);
+        }
+    }
+
+    private void match(RuleReference ruleReference, List<ParseTree> parseTrees) {
+        if (parseTrees.size() == 0) {
+            if (ruleReference.quantity == Quantity.AT_LEAST_ONE || ruleReference.quantity == Quantity.EXACTLY_ONE) {
+                throw new IllegalArgumentException("Matching was not successful");
+            } else {
+                // match was successful by matching nothing
+                return;
+            }
+        }
+        Quantity quantity = ruleReference.quantity;
+        premun.mps.ingrid.model.Rule rule = ruleReference.rule;
+        if (rule instanceof ParserRule && rule.name.contains("_block_")) {
+            List<Alternative> alternatives = ((ParserRule) rule).alternatives;
+            Alternative appropriateAlternative = FormatExtractorUtils.findAppropriateAlternative(alternatives, parseTrees, Arrays.asList(grammar.getRuleNames()));
+            ArrayList<ParseTree> copy = new ArrayList<>(parseTrees);
+            if (quantity == Quantity.MAX_ONE || quantity == Quantity.EXACTLY_ONE || quantity == Quantity.AT_LEAST_ONE) {
+                try {
+                    extractFormatInfo(copy, appropriateAlternative.elements);
+                } catch (IllegalArgumentException ex) {
+                    if (quantity == Quantity.EXACTLY_ONE || quantity == Quantity.AT_LEAST_ONE) {
+                        throw new IllegalArgumentException("Matching was not successful");
+                    }
+                }
+                if (quantity == Quantity.EXACTLY_ONE) {
+                    extractFormatInfo(parseTrees, appropriateAlternative.elements);
+                    return;
+                }
+                if (quantity == Quantity.AT_LEAST_ONE) {
+                    extractFormatInfo(parseTrees, appropriateAlternative.elements);
+                }
+            }
+            while (true) {
+                copy = new ArrayList<>(parseTrees);
+                try {
+                    // first try on copy, if it works, try on original as well
+                    extractFormatInfo(copy, appropriateAlternative.elements);
+                    extractFormatInfo(parseTrees, appropriateAlternative.elements);
+                } catch (IllegalArgumentException ex) {
+                    // FIXME using exception for control flow, bleh...needs refactoring
+                    break;
+                }
+            }
+            return;
+
+        }
+        switch (quantity) {
+            case MAX_ONE:
+                if (matches(rule, parseTrees.get(0))) {
+                    parseTrees.remove(0);
+                }
+                break;
+            case EXACTLY_ONE:
+                if (matches(rule, parseTrees.get(0))) {
+                    parseTrees.remove(0);
+                } else {
+                    throw new IllegalArgumentException("Matching was not successful");
+                }
+                break;
+            case AT_LEAST_ONE:
+                if (!matches(rule, parseTrees.get(0))) {
+                    throw new IllegalArgumentException("Matching was not successful");
+                }
+                parseTrees.remove(0);
+                while (matches(rule, parseTrees.get(0))) {
+                    parseTrees.remove(0);
+                }
+                break;
+            case ANY:
+                while (matches(rule, parseTrees.get(0))) {
+                    parseTrees.remove(0);
+                }
+                break;
+        }
+
+    }
+
+    private boolean matches(premun.mps.ingrid.model.Rule rule, ParseTree parseTree) {
+        if (rule instanceof LiteralRule) {
+            if (!(parseTree instanceof TerminalNode)) {
+                return false;
+            } else {
+                return ((TerminalNode) parseTree).getSymbol()
+                                                 .getText()
+                                                 .equals(((LiteralRule) rule).value);
+            }
+        } else if (rule instanceof RegexRule) {
+            if (!(parseTree instanceof TerminalNode)) {
+                return false;
+            } else {
+                return ((TerminalNode) parseTree).getSymbol()
+                                                 .getText()
+                                                 .matches(((RegexRule) rule).regexp);
+            }
+        } else if (rule instanceof ParserRule) {
+            if (parseTree instanceof TerminalNode) {
+                return false;
+            } else if (!(parseTree instanceof RuleContext)) {
+                throw new IllegalArgumentException("Unknown type of ParseTree node");
+            } else {
+                return rule.name.equals(grammar.getRule(((RuleContext) parseTree).getRuleIndex()).name);
+            }
+        } else {
+            throw new IllegalArgumentException("Unknown type of rule");
+        }
+    }
+
+    private List<RuleReference> expandBlockRules(List<RuleReference> elements) {
+        // we need to expand block rules created by ingrid, because they do not exist in the original grammar
+        // and therefore enter every rule will not be called for them
+        // as a simple fix, we can find them by looking for substring _block_ in their name
+        // if we find one of these, we expand them manually
+        List<RuleReference> result = new ArrayList<>();
+        for (RuleReference elem : elements) {
+            if ((elem.rule instanceof ParserRule) && elem.rule.name.contains("_block_")) {
+                if (((ParserRule) elem.rule).alternatives.size() != 1) {
+                    throw new IllegalArgumentException("Block rule should only have one alternative");
+                }
+                result.addAll(expandBlockRules(((ParserRule) elem.rule).alternatives.get(0).elements));
+            } else {
+                result.add(elem);
+            }
+        }
+        return result;
+    }
+
+    private Token extractToken(ParseTree parseTree, Function<ParseTree, ParseTree> nextNodeExtractor) {
+        while (!(parseTree instanceof TerminalNode)) {
+            if (parseTree.getChildCount() == 0) {
+                throw new IllegalArgumentException("Cannot find leftmost token in this tree");
+            }
+            parseTree = nextNodeExtractor.apply(parseTree);
+        }
+        return ((TerminalNode) parseTree).getSymbol();
     }
 
     private String serializeAlternative(Alternative alternative) {
