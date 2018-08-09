e6f52315fb7a5777fc3b5dee7af4536566f9f94a
dkozak <dkozak94@gmail.com>
2018-08-09 17:43:54 +0200

    add tests for the expr lang

--- plugin/idea/formatter/test/premun/mps/ingrid/formatter/FormatExtractorBasicTest.java
+++ plugin/idea/formatter/test/premun/mps/ingrid/formatter/FormatExtractorBasicTest.java
@@ -1,6 +1,5 @@
 package premun.mps.ingrid.formatter;
 
-import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 import premun.mps.ingrid.formatter.boundary.FormatExtractor;
 import premun.mps.ingrid.formatter.model.RuleFormatInfo;
@@ -30,7 +29,7 @@ import static premun.mps.ingrid.formatter.utils.FormatInfoMapDump.dumpSimplified
 public class FormatExtractorBasicTest {
 
     @Test
-    public void setGrammarEmptySet() throws RecognitionException {
+    public void setGrammarEmptySet() {
         Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("{}", TestGrammars.setGrammar);
 
         dumpSimplifiedMap(formatInfoMap);
@@ -54,7 +53,7 @@ public class FormatExtractorBasicTest {
     }
 
     @Test
-    public void setGrammarSimpleInput() throws RecognitionException {
+    public void setGrammarSimpleInput() {
         Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("{1,2,3}", TestGrammars.setGrammar);
 
         dumpSimplifiedMap(formatInfoMap);
@@ -99,7 +98,7 @@ public class FormatExtractorBasicTest {
     }
 
     @Test
-    public void setGrammarNestedInput() throws RecognitionException {
+    public void setGrammarNestedInput() {
         Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("{1,{a,b,c},3}", TestGrammars.setGrammar);
 
         dumpSimplifiedMap(formatInfoMap);
@@ -149,7 +148,7 @@ public class FormatExtractorBasicTest {
     }
 
     @Test
-    public void setGrammarNestedInputMoreComplex() throws RecognitionException {
+    public void setGrammarNestedInputMoreComplex() {
         Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("{1,{a,b,c},{{},{a,b,c}}}", TestGrammars.setGrammar);
 
         dumpSimplifiedMap(formatInfoMap);
@@ -206,7 +205,7 @@ public class FormatExtractorBasicTest {
     }
 
     @Test
-    public void setGrammarFormatted() throws RecognitionException {
+    public void setGrammarFormatted() {
         String input = "{\n" +
                 "\ta,\n" +
                 "\t{a,b,c},\n" +
@@ -261,7 +260,7 @@ public class FormatExtractorBasicTest {
     }
 
     @Test
-    public void setGrammarSimpleFormat() throws RecognitionException {
+    public void setGrammarSimpleFormat() {
         String input = "{\n" +
                 "  a,\n" +
                 "  b,\n" +
@@ -312,20 +311,92 @@ public class FormatExtractorBasicTest {
 
 
     @Test
-    public void expressionGrammarVerySimple() throws RecognitionException {
+    public void expressionGrammarVerySimple() {
         Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("(1 + 1) * 2", TestGrammars.expressionGrammar);
         dumpSimplifiedMap(formatInfoMap);
+
+        verifyFormatInfoMap(
+                formatInfoMap,
+                rules(
+                        rule(
+                                "expr", 5,
+                                handle(
+                                        elem("INT", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        ),
+                        rule(
+                                "expr", 3,
+                                handle(
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("+", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        ),
+                        rule(
+                                "expr", 1,
+                                handle(
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("*", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        ),
+                        rule(
+                                "expr", 0,
+                                handle(
+                                        elem("(", newLine(false), space(false), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("expr", newLine(false), space(false), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem(")", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        )
+                )
+        );
     }
 
     @Test
-    public void expressionGrammarMoreComplex() throws RecognitionException {
-        Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("((2*1) + 1) * 2", TestGrammars.expressionGrammar);
+    public void expressionGrammarMoreComplex() {
+        Map<Pair<String, Integer>, RuleFormatInfo> formatInfoMap = extractFormat("((2*1) + 1)*2", TestGrammars.expressionGrammar);
         dumpSimplifiedMap(formatInfoMap);
+
+        verifyFormatInfoMap(
+                formatInfoMap,
+                rules(
+                        rule(
+                                "expr", 5,
+                                handle(
+                                        elem("INT", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        ),
+                        rule(
+                                "expr", 3,
+                                handle(
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("+", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        ),
+                        rule(
+                                "expr", 1,
+                                handle(
+                                        elem("expr", newLine(false), space(false), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("*", newLine(false), space(false), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("expr", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        ),
+                        rule(
+                                "expr", 0,
+                                handle(
+                                        elem("(", newLine(false), space(false), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem("expr", newLine(false), space(false), childrenOnNewLine(false), childrenIndented(false)),
+                                        elem(")", newLine(false), space(true), childrenOnNewLine(false), childrenIndented(false))
+                                )
+                        )
+                )
+        );
     }
 
 
     @Test
-    public void cimpleFibonacci() throws RecognitionException {
+    public void cimpleFibonacci() {
         String input = "fn fib(i){\n" +
                 "    if(i < 2){\n" +
                 "        return 1;\n" +
@@ -349,7 +420,7 @@ public class FormatExtractorBasicTest {
 
 
     @Test
-    public void cimpleFibonacciMinimalFormat() throws RecognitionException {
+    public void cimpleFibonacciMinimalFormat() {
         String input = "fn fib(i){if(i < 2){return 1;}else{return fib(i - 1)  + fib(i - 2);}}\n" +
                 "i = 10;\n" +
                 "for(j = 0; j < i ; j = j+1){res = fib(j);print res;}";
@@ -362,7 +433,7 @@ public class FormatExtractorBasicTest {
 
 
     @Test
-    public void cimpleFibonacciNoFormat() throws RecognitionException {
+    public void cimpleFibonacciNoFormat() {
         String input = "fn fib(i){if(i < 2){return 1;}else{return fib(i - 1) + fib(i - 2);}} i = 10; for(j = 0; j < i ; j = j+1){res = fib(j);print res;}";
 
         String cimple = TestGrammars.loadCimple();
@@ -372,7 +443,7 @@ public class FormatExtractorBasicTest {
     }
 
 
-    private Map<Pair<String, Integer>, RuleFormatInfo> extractFormat(String input, String grammar) throws RecognitionException {
+    private Map<Pair<String, Integer>, RuleFormatInfo> extractFormat(String input, String grammar) {
         GrammarParser grammarParser = new GrammarParser();
         grammarParser.parseString(grammar);
         GrammarInfo grammarInfo = grammarParser.resolveGrammar();
